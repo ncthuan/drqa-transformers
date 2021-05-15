addadf8a135c3ac26e2e79637f80c78e24d70ab4
ncthuan <ncthuan.it@gmail.com>
2021-05-15 15:47:39 +0700

    implement batch inference to prevent gpu memory overflow

--- drqa/pipeline/drqa_transformers.py
+++ drqa/pipeline/drqa_transformers.py
@@ -2,6 +2,7 @@ import torch
 import regex
 import logging
 import numpy as np
+from math import ceil
 
 from multiprocessing import Pool as ProcessPool
 from multiprocessing.util import Finalize
@@ -46,12 +47,12 @@ class DrQATransformers(object):
             self,
             reader_model=None,
             use_fast_tokenizer=True,
+            group_length=200,
             batch_size=32,
             cuda=True,
             num_workers=None,
             db_config=None,
             ranker_config=None,
-            group_length=200,
         ):
         """Initialize the pipeline.
 
@@ -155,15 +156,27 @@ class DrQATransformers(object):
             return_tensors='pt'
         ).to(self.device)
         
-        # Feed forward
-        n_batch = (n_examples // self.batch_size) + 1
+        # Split batches
+        n_batch = ceil(n_examples / self.batch_size)
+        batches = [i*self.batch_size for i in range(n_batch)] #[0,32,64,..]
+        batches.append(n_examples)
+
+        # Feed forward batches
+        outputs = []
         for i in range(n_batch):
-            pass
-        with torch.no_grad():
-            outputs = self.reader(**inputs)
+            with torch.no_grad():
+                output = self.reader(
+                    inputs.input_ids[batches[i]:batches[i+1]],
+                    inputs.attention_mask[batches[i]:batches[i+1]]
+                )
+                outputs.append(output)
+
+        # Join batch outputs
+        start_logits = torch.cat([o.start_logits for o in outputs], dim=0)
+        end_logits = torch.cat([o.end_logits for o in outputs], dim=0)
 
         # decode start-end logits to span slice & score
-        start, end, score, idx_sort = self.decode_transformers(inputs, outputs, topk=top_n)
+        start, end, score, idx_sort = self.decode_transformers(inputs, start_logits, end_logits, topk=top_n)
 
         # Produce predictions, take top_n predictions with highest score
         all_predictions = []
@@ -192,7 +205,7 @@ class DrQATransformers(object):
         return all_predictions
 
 
-    def decode_transformers(self, inputs, outputs, topk=1, max_answer_len=None):
+    def decode_transformers(self, inputs, start_logits, end_logits, topk=1, max_answer_len=None):
         """
         Take the output of any :obj:`ModelForQuestionAnswering` and generate probabilities for each span to be the
         actual answer.
@@ -202,8 +215,9 @@ class DrQATransformers(object):
         the topk argument.
 
         Args:
-            # start (:obj:`np.ndarray`): Individual start logits for each token. # shape batch*len(input_ids[0])
-            # end (:obj:`np.ndarray`): Individual end logits for each token.
+            inputs: inputs object outputted by the tokenizer
+            start_logits (:obj:`tensor`): Individual start logits for each token. # shape batch*len(input_ids[0])
+            end_logits (:obj:`tensor`): Individual end logits for each token.
             topk (:obj:`int`): Indicates how many possible answer span(s) to extract from the model output.
             max_answer_len (:obj:`int`): Maximum size of the answer to extract from the model's output.
             undesired_tokens (:obj:`np.ndarray`): Mask determining tokens that can be part of the answer
@@ -214,12 +228,12 @@ class DrQATransformers(object):
             idx_sort:  top_n batch element ids
         """
         input_ids = inputs.input_ids.numpy()
-        start = outputs.start_logits.numpy()
-        end = outputs.end_logits.numpy()
+        start_logits = start_logits.numpy()
+        end_logits = end_logits.numpy()
         # Ensure we have batch axis
-        if start.ndim == 1: start = start[None]
-        if end.ndim == 1: end = end[None]
-        max_answer_len = max_answer_len or start.shape[1]
+        if start_logits.ndim == 1: start_logits = start_logits[None]
+        if end_logits.ndim == 1: end_logits = end_logits[None]
+        max_answer_len = max_answer_len or start_logits.shape[1]
 
         # Ensure padded tokens & question tokens cannot belong to the set of candidate answers.
         # We need the part between two <SEP> tokens
@@ -232,12 +246,12 @@ class DrQATransformers(object):
         undesired_tokens_mask = undesired_tokens == 0
 
         # Make sure non-context indexes in the tensor cannot contribute to the softmax
-        start = np.where(undesired_tokens_mask, -10000.0, start)
-        end = np.where(undesired_tokens_mask, -10000.0, end)
+        start_logits = np.where(undesired_tokens_mask, -10000.0, start_logits)
+        end_logits = np.where(undesired_tokens_mask, -10000.0, end_logits)
 
-        # Normalize logits and spans to retrieve the answer
-        start = np.exp(start - np.log(np.sum(np.exp(start), axis=-1, keepdims=True)))
-        end = np.exp(end - np.log(np.sum(np.exp(end), axis=-1, keepdims=True)))
+        # Normalize logits to probs
+        start = np.exp(start_logits - np.log(np.sum(np.exp(start_logits), axis=-1, keepdims=True)))
+        end = np.exp(end_logits - np.log(np.sum(np.exp(end_logits), axis=-1, keepdims=True)))
 
         # Compute the score of each tuple(start, end) to be the real answer
         outer = np.matmul(np.expand_dims(start, -1), np.expand_dims(end, 1))
